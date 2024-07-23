9c372872f48c0fdd66bd71b74e04a3d2629e8486
osamajamal11 <osamajamal11@gmail.com>
2024-07-24 01:42:30 +0300

    last touches

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ result.setAttribute("value" , result.getAttribute("value")+input);
 
 }
 
+function opDisplay(input){
+    var op = ["." , "+" , "-" , "*" , "/" , "%"]
+    var last = result.getAttribute("value").length-1;
+    if(op.includes(result.getAttribute("value")[last]) && result.getAttribute("value")[last] === input ){
+        return;
+    }
+    else if(op.includes(result.getAttribute("value")[last]) && result.getAttribute("value")[last] !== input ){
+        result.setAttribute("value" , result.getAttribute("value").slice(0,last)+input);
+    }
+    else{
+    result.setAttribute("value" , result.getAttribute("value")+input);
+    }
+}
+
 function clearResult(){
 
     result.setAttribute("value" , "");
